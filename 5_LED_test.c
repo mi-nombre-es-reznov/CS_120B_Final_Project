c566512737a97eab0756459923a8b46cd4a63088
Nicholas <npere017@ucr.edu>
2018-05-24 17:50:11 +0000

    Working prototype with 5 LEDs. Need to fix errors now and increase to 16 LEDs.

---
+++ 5_LED_test.c
@@ -0,0 +1,217 @@
+/*
+ * npere017_FP_LED_Lights_with_electro-things.c
+ *
+ * Created: 5/22/2018 10:22:22 AM
+ * Author : xXSexybeastXx
+ */ 
+
+#include <avr/io.h>
+#include "timer.h"
+#include "io.c"
+#include "io.h"
+
+// Values
+unsigned char curr;
+unsigned char score = 0;
+
+enum LED_States{LED_SMStart, LED_Init, LED_L1, LED_L2, LED_L3, LED_L4, LED_L5, LED_Wrong, LED_Right, LED_Release} LED_state;
+    
+void LEDs()
+{
+    switch(LED_state)
+    {
+        case(LED_SMStart):
+        {
+            LED_state = LED_Init;
+            break;
+        }
+        case(LED_Init):
+        {
+            LED_state = LED_L1;
+            break;
+        }
+        case(LED_L1):
+        {
+            curr = LED_state;
+            
+            if((~PINA & 0x04) == 0x04)
+            {
+                LED_state = LED_Wrong;
+            }
+            else
+            {
+                LED_state = LED_L2;
+            }
+            
+            break;
+        }
+        case(LED_L2):
+        {
+            curr = LED_state;
+            
+            if((~PINA & 0x04) == 0x04)
+            {
+                LED_state = LED_Right;
+            }
+            else
+            {
+                LED_state = LED_L3;
+            }
+            
+            break;
+        }
+        case(LED_L3):
+        {
+            curr = LED_state;
+            
+            if((~PINA & 0x04) == 0x04)
+            {
+                LED_state = LED_Wrong;
+            }
+            else
+            {
+                LED_state = LED_L4;
+            }
+            
+            break;
+        }
+        case(LED_L4):
+        {
+            curr = LED_state;
+            
+            if((~PINA & 0x04) == 0x04)
+            {
+                LED_state = LED_Right;
+            }
+            else
+            {
+                LED_state = LED_L5;
+            }
+            
+            break;
+        }
+        case(LED_L5):
+        {
+            curr = LED_state;
+            
+            if((~PINA & 0x04) == 0x04)
+            {
+                LED_state = LED_Wrong;
+            }
+            else
+            {
+                LED_state = LED_L1;
+            }
+            
+            break;
+        }
+        case(LED_Wrong):
+        {
+            LED_state = LED_Release;
+            break;
+        }
+        case(LED_Right):
+        {
+            LED_state = LED_Release;
+            break;
+        }
+        case(LED_Release):
+        {
+            if((~PINA & 0x04) == 0x00)
+            {
+                LED_state = curr;
+            }
+            else
+            {
+                LED_state = LED_Release;
+            }
+            
+            break;
+        }
+        default:
+        {
+            LED_state = LED_Init;
+        }
+    }
+    
+    switch(LED_state)
+    {
+        case(LED_SMStart):
+        {
+            break;
+        }
+        case(LED_Init):
+        {
+            score = 0;
+            curr = LED_Init;
+            LCD_WriteData(score + '0');
+            break;
+        }
+        case(LED_L1):
+        {
+            PORTB = 0x01;
+            break;
+        }         
+        case(LED_L2):
+        {
+            PORTB = 0x02;
+            break;
+        }   
+        case(LED_L3):
+        {
+            PORTB = 0x04;
+            break;
+        }
+        case(LED_L4):
+        {
+            PORTB = 0x08;
+            break;
+        }
+        case(LED_L5):
+        {
+            PORTB = 0x10;
+            break;
+        }
+        case(LED_Wrong):
+        {
+            score = (score - 1);
+            LCD_WriteData(score + '0');
+            break;
+        }
+        case(LED_Right):
+        {
+            score = (score + 1);
+            LCD_WriteData(score + '0');
+            break;
+        }
+        case(LED_Release):
+        {
+            break;
+        }
+        default:
+        {
+            LCD_DisplayString(6, "ERROR");
+            break;
+        }
+    }
+}
+
+int main(void)
+{
+    DDRA = 0x03;    PORTA = 0xFC;
+    DDRD = 0xFF;    PORTD = 0x00;
+    DDRB = 0xFF;    PORTB = 0x00;
+    LCD_init();
+    LCD_ClearScreen();
+    
+    TimerSet(500);
+    TimerOn();
+    /* Replace with your application code */
+    while (1) 
+    {
+        LEDs();
+        
+        while(!TimerFlag);
+        TimerFlag = 0;
+    }
+}

7460c119cbc005b3a6b3a3924026366e2ffe6e54
Nicholas <npere017@ucr.edu>
2018-05-26 18:52:07 +0000

    A setup of a compilable code, without implementation of the LCD display

--- Beginning_Messages.c
+++ Beginning_Messages.c
@@ -4,3 +4,16 @@
 #include <avr/interrupt.h>
 #include "io.c"
 #include "io.h"
+ 
+enum Intro_LCD{Intro_SMStart, Intro_Init, Intro_Stage1, Intro_Stage2, Intro_Stage3, Intro_Stage4,
+    Intro_Stage5, Intro_Stage6, Intro_Stage7, Intro_Stage8, Intro_Stage9, Intro_Stage_A, Intro_StageB, Intro_StageC} Intro_state;
+    
+
+    
+int main()
+{
+    while(1)
+    {
+        
+    }
+}

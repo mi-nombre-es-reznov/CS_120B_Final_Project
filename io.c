7708cd2ef61e2a6503a9088703ea58364adc1d09
Nicholas <npere017@ucr.edu>
2018-05-29 22:09:35 +0000

    Added code in io.c and io.h for custom character creation

--- io.c
+++ io.c
@@ -6,6 +6,16 @@
 #define SET_BIT(p,i) ((p) |= (1 << (i)))
 #define CLR_BIT(p,i) ((p) &= ~(1 << (i)))
 #define GET_BIT(p,i) ((p) & (1 << (i)))
+
+
+// More code for the LCD creator
+#define LCD_Data_Dir DDRD	/* Define LCD data port direction */
+#define LCD_Command_Dir DDRA	/* Define LCD command port direction register */
+#define LCD_Data_Port PORTD	/* Define LCD data port */
+#define LCD_Command_Port PORTA	/* Define LCD data port */
+#define RS PA0			/* Define Register Select signal pin */
+#define RW PA1			/* Define Read/Write signal pin */
+#define EN PA2			/* Define Enable signal pin */
           
 /*-------------------------------------------------------------------------*/
 
@@ -77,4 +87,77 @@ void delay_ms(int miliSec) //for 8 Mhz crystal
   {
    asm("nop");
   }
+}
+
+
+// To make custom character work
+void LCD_Init (void)			/* LCD Initialize function */
+{
+    LCD_Command_Dir = 0xFF;		/* Make LCD command port direction as o/p */
+    LCD_Data_Dir = 0xFF;		/* Make LCD data port direction as o/p */
+    _delay_ms(20);			/* LCD Power ON delay always >15ms */
+    
+    LCD_Command (0x38);		/* Initialization of 16X2 LCD in 8bit mode */
+    LCD_Command (0x0C);		/* Display ON Cursor OFF */
+    LCD_Command (0x06);		/* Auto Increment cursor */
+    LCD_Command (0x01);		/* clear display */
+    _delay_ms(2);			/* Clear display command delay> 1.63 ms */
+    LCD_Command (0x80);		/* Cursor at home position */
+}
+
+void LCD_Command(unsigned char cmnd)
+{
+    LCD_Data_Port= cmnd;
+    LCD_Command_Port &= ~(1<<RS);	/* RS=0 command reg. */
+    LCD_Command_Port &= ~(1<<RW);	/* RW=0 Write operation */
+    LCD_Command_Port |= (1<<EN);	/* Enable pulse */
+    _delay_us(1);
+    LCD_Command_Port &= ~(1<<EN);
+    _delay_ms(2);
+}
+
+void LCD_Char (unsigned char char_data)  /* LCD data write function */
+{
+    LCD_Data_Port= char_data;
+    LCD_Command_Port |= (1<<RS);	/* RS=1 Data reg. */
+    LCD_Command_Port &= ~(1<<RW);	/* RW=0 write operation */
+    LCD_Command_Port |= (1<<EN);	/* Enable Pulse */
+    _delay_us(1);
+    LCD_Command_Port &= ~(1<<EN);
+    _delay_ms(2);			/* Data write delay */
+}
+
+void LCD_String (char *str)		/* Send string to LCD function */
+{
+    int i;
+    for(i=0;str[i]!=0;i++)		/* Send each char of string till the NULL */
+    {
+        LCD_Char (str[i]);
+    }
+}
+
+void LCD_String_xy (char row, char pos, char *str)  /* Send string to LCD with xy position */
+{
+    if (row == 0 && pos<16)
+    LCD_Command((pos & 0x0F)|0x80);	/* Command of first row and required position<16 */
+    else if (row == 1 && pos<16)
+    LCD_Command((pos & 0x0F)|0xC0);	/* Command of first row and required position<16 */
+    LCD_String(str);		/* Call LCD string function */
+}
+
+void LCD_Clear()
+{
+    LCD_Command (0x01);		/* clear display */
+    LCD_Command (0x80);		/* cursor at home position */
+}
+
+void LCD_Custom_Char (unsigned char loc, unsigned char *msg)
+{
+    unsigned char i;
+    if(loc<8)
+    {
+        LCD_Command (0x40 + (loc*8));	/* Command 0x40 and onwards forces the device to point CGRAM address */
+        for(i=0;i<8;i++)	/* Write 8 byte for generation of 1 character */
+        LCD_Char(msg[i]);
+    }
 }
--- io.h
+++ io.h
@@ -7,5 +7,13 @@ void LCD_WriteCommand (unsigned char Command);
 void LCD_Cursor (unsigned char column);
 void LCD_DisplayString(unsigned char column ,const unsigned char *string);
 void delay_ms(int miliSec);
-#endif
 
+// Other code for LCD character creation
+void LCD_INIT();
+void LCD_Command(unsigned char);
+void LCD_Char (unsigned char char_data);
+void LCD_String (char *str);
+void LCD_String_xy (char row, char pos, char *str);
+void LCD_Clear();
+void LCD_Custom_Char (unsigned char loc, unsigned char *msg);
+#endif

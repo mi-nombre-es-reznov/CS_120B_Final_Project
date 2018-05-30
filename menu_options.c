59aec995caed5e94ec75bdc22154a4a2967e54d2
Nicholas <npere017@ucr.edu>
2018-05-30 02:15:12 +0000

    Init of menu options

---
+++ menu_options.c
@@ -0,0 +1,11 @@
+// This code will be the menu options after the initial start button has been pressed!
+
+#include <avr/io.h>
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}

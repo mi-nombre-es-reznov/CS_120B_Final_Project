b0369063f404238ca89b0f157e3b122472c6ddad
Nicholas <npere017@ucr.edu>
2018-05-30 09:25:40 +0000

    There are many errors in this

--- menu_options.c
+++ menu_options.c
@@ -1,11 +1,304 @@
-// This code will be the menu options after the initial start button has been pressed!
+// Joystick code test to allow for signals of left, right, up, and down.
+// The code even accepts the button press from pressing the joystick in.
 
+#define F_CPU 8000000UL
 #include <avr/io.h>
+#include <util/delay.h>
+#include <stdlib.h>
+#include "adc.h"
+#include "adc.c"
+#include "timer.h"
+#include "io.h"
+#include "io.c"
 
-int main(void)
+// Created characters for LCD screen
+unsigned char Character1[8] = { 0x1f,0x11,0x15,0x15,0x15,0x15,0x11,0x1f };    // Unpressed button
+unsigned char Character2[8] = { 0x0,0xe,0xe,0xe,0xe,0xe,0xe,0x0 };            // Pressed button
+unsigned char Character3[8] = { 0x0,0x4,0x6,0x1f,0x6,0x4,0x0,0x0 };           // Right Arrow
+unsigned char Character4[8] = { 0x0,0x4,0xc,0x1f,0xc,0x4,0x0,0x0 };           // Left Arrow
+unsigned char Character5[8] = { 0x4,0xe,0xe,0x1f,0x1f,0x4,0x4,0x4 };          // Up Arrow
+unsigned char Character6[8] = { 0x4,0x4,0x4,0x1f,0x1f,0xe,0xe,0x4 };          // Down Arrow
+unsigned char Character7[8] = { 0x11,0x11,0x11,0x11,0x1f,0x1f,0x1f,0x1f };    // Hand
+unsigned char Character8[8] = { 0x1f,0x11,0x11,0x11,0x11,0x1b,0x1b,0x1b };    // Headphones
+    
+// Global variables
+unsigned long x, y = 0;
+unsigned char whole, half, quarter, eighth, sixteenth, performance, pos, left, right, up, down, same, start, curr;
+
+enum M_States {M_SMStart, M_Init, M_X, M_Y, M_Training, M_Whole, M_Half, M_Quarter, M_Eighth, M_Sixteenth, M_Performance} state;
+
+void Menu()
 {
-    /* Replace with your application code */
-    while (1) 
+    unsigned long temp = 50;
+    unsigned char tempb = 0;
+    
+    switch(state)
     {
+        case(M_SMStart):
+        {
+            state = M_Init;
+            break;
+        }
+        case(M_Init):
+        {
+            curr = M_Training;
+            state = M_X;
+            break;
+        }
+        case(M_X):
+        {
+            state = M_Y;
+            break;
+        }
+        case(M_Y):
+        {
+            state = curr;
+            break;
+        }
+        case(M_Training):
+        {
+            if(pos == up)
+            {
+                state = M_Whole;
+            }
+            else if(pos == right)
+            {
+                state = M_Performance;
+            }
+            else
+            {
+                state = M_X;
+            }
+            
+            break;
+        }
+        case(M_Whole):
+        {
+            if(pos == up)
+            {
+                state = M_Half;
+            }
+            else if(pos == down)
+            {
+                state = M_Training;
+            }
+            else
+            {
+                state = M_X;
+            }
+            
+            break;
+        }
+        case(M_Half):
+        {
+            if(pos == up)
+            {
+                state = M_Quarter;
+            }
+            else if(pos == down)
+            {
+                state = M_Whole;
+            }
+            else
+            {
+                state = M_X;
+            }
+            
+            break;
+        }
+        case(M_Quarter):
+        {
+            if(pos == up)
+            {
+                state = M_Eighth;
+            }            
+            else if(pos == down)
+            {
+                state = M_Half;
+            }
+            else
+            {
+                state = M_X;
+            }
+            
+            break;
+        }
+        case(M_Eighth):
+        {
+            if(pos == up)
+            {
+                state = M_Sixteenth;
+            }
+            else if(pos == down)
+            {
+                state = M_Quarter;
+            }
+            else
+            {
+                state = M_X;
+            }
+            
+            break;
+        }
+        case(M_Sixteenth):
+        {
+            if(pos == down)
+            {
+                state = M_Eighth;
+            }
+            else
+            {
+                state = M_X;
+            }
+            
+            break;
+        }
+        case(M_Performance):
+        {
+            if(pos == left)
+            {
+                state = M_Training;
+            }
+            else
+            {
+                state = M_X;
+            }
+            
+            break;
+        }
+        default:
+        {
+            state = M_Init;
+            break;
+        }
     }
+    
+    switch(state)
+    {
+        case(M_SMStart):
+        {
+            break;
+        }
+        case(M_Init):
+        {
+            PORTB = 0x00;
+            whole = 0;
+            half = 0;
+            quarter = 0;
+            eighth = 0;
+            sixteenth = 0;
+            performance = 0;
+            LCD_Clear();
+            break;
+        }
+        case(M_X):
+        {
+            x = ADC_ReadData(3);
+            if (x >= 0 && x < 2*temp)
+            {
+                pos = left;
+            }
+            else if(x >= 4*temp && x < 6*temp)
+            {
+                pos = right;
+            }
+            else if (x >= 3*temp && x < 6*temp)
+            {
+                pos = same;
+            }
+            
+            if((~PINA & 0x20) == 0x20)
+            {
+                pos = start;
+            }
+            
+            break;
+        }
+        case(M_Y):
+        {
+            y = ADC_ReadData(4);
+
+            if(y < 30)
+            {
+                pos = up;
+            }
+            else if(y >= 4*temp && y < 6*temp)
+            {
+                pos = down;
+            }
+
+            if((~PINA & 0x20) == 0x20)
+            {
+                pos = start;
+            }
+        }
+        case(M_Training):
+        {   
+            LCD_Clear(); 
+            LCD_Char(' ');
+            LCD_Char(' ');
+            LCD_String(" Training??");
+            LCD_Char(' ');
+            LCD_Char(' ');
+            LCD_Char(' ');
+            LCD_Char(' ');
+            LCD_Char(' ');
+            LCD_Char(' ');
+            LCD_Char(' ');
+            LCD_Char(' ');
+            LCD_Char(' ');
+            LCD_Char(' ');
+            LCD_Char(' ');
+            LCD_Char(' ');
+            LCD_Char(' ');
+            LCD_Char(2);
+
+            break;
+        }
+        case(M_Whole):
+        {
+            LCD_Clear();
+            LCD_String("     Whole            Notes!");
+            break;
+        }            
+    }
+}
+
+int main()
+{
+    //DDRA = 0x07; PORTA = 0xF8;
+    DDRB = 0xFF; PORTB = 0x00;
+    DDRD = 0xFF; PORTD = 0x00;
+    
+    ADC_Init(5, 1, 1);
+
+    LCD_Custom_Char(0, Character1);  /* Build Character1 at position 0 */
+    LCD_Custom_Char(1, Character2);  /* Build Character2 at position 1 */
+    LCD_Custom_Char(2, Character3);  /* Build Character3 at position 2 */
+    LCD_Custom_Char(3, Character4);  /* Build Character4 at position 3 */
+    LCD_Custom_Char(4, Character5);  /* Build Character5 at position 4 */
+    LCD_Custom_Char(5, Character6);  /* Build Character6 at position 5 */
+    LCD_Custom_Char(6, Character7);  /* Build Character6 at position 6 */
+    LCD_Custom_Char(7, Character8);  /* Build Character6 at position 7 */
+
+    LCD_Command(0x80);		/*cursor at home position */
+    LCD_Command(0xc0);    
+    
+    // Set the timer and turn it on
+    TimerSet(350);
+    TimerOn();
+    LCD_Init();
+    LCD_ClearScreen();
+
+    unsigned short i; // Scheduler for-loop iterator
+
+    while(1)
+    {
+        Menu();
+        
+        while(!TimerFlag);
+        TimerFlag = 0;
+    }
+    // Error: Program should not exit!
+    return 0;
 }

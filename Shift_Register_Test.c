9871c51743bc3077f4c17cece3a271f1b2f75318
Nicholas <npere017@ucr.edu>
2018-06-01 03:24:45 +0000

    Init of Shift Regsiter Test

---
+++ Shift_Register_Test.c
@@ -0,0 +1,13 @@
+// This is a test area to try and figure out how to use shift registers.
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+

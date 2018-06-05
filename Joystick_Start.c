98879f4e03386c289f691911a44cc71fbf57f584
Nicholas <npere017@ucr.edu>
2018-06-05 08:47:13 +0000

    Updated timings on Start Menu

--- Joystick_Start.c
+++ Joystick_Start.c
@@ -9,17 +9,17 @@
 #include "timer.h"
 
 // Created characters for LCD screen
-	unsigned char Character1[8] = { 0x1f,0x11,0x15,0x15,0x15,0x15,0x11,0x1f };    // Unpressed button
-	unsigned char Character2[8] = { 0x0,0xe,0xe,0xe,0xe,0xe,0xe,0x0 };            // Pressed button
-	unsigned char Character3[8] = { 0x0,0x4,0x6,0x1f,0x6,0x4,0x0,0x0 };           // Right Arrow
-	unsigned char Character4[8] = { 0x0,0x4,0xc,0x1f,0xc,0x4,0x0,0x0 };           // Left Arrow
-	unsigned char Character5[8] = { 0x4,0xe,0xe,0x1f,0x1f,0x4,0x4,0x4 };          // Up Arrow
-	unsigned char Character6[8] = { 0x4,0x4,0x4,0x1f,0x1f,0xe,0xe,0x4 };          // Down Arrow
-	unsigned char Character7[8] = { 0x11,0x11,0x11,0x11,0x1f,0x1f,0x1f,0x1f };    // Hand
-	unsigned char Character8[8] = { 0x1f,0x11,0x11,0x11,0x11,0x1b,0x1b,0x1b };    // Headphones
-        
+unsigned char Character1[8] = { 0x1f,0x11,0x15,0x15,0x15,0x15,0x11,0x1f };    // Unpressed button
+unsigned char Character2[8] = { 0x0,0xe,0xe,0xe,0xe,0xe,0xe,0x0 };            // Pressed button
+unsigned char Character3[8] = { 0x0,0x4,0x6,0x1f,0x6,0x4,0x0,0x0 };           // Right Arrow
+unsigned char Character4[8] = { 0x0,0x4,0xc,0x1f,0xc,0x4,0x0,0x0 };           // Left Arrow
+unsigned char Character5[8] = { 0x4,0xe,0xe,0x1f,0x1f,0x4,0x4,0x4 };          // Up Arrow
+unsigned char Character6[8] = { 0x4,0x4,0x4,0x1f,0x1f,0xe,0xe,0x4 };          // Down Arrow
+unsigned char Character7[8] = { 0x11,0x11,0x11,0x11,0x1f,0x1f,0x1f,0x1f };    // Hand
+unsigned char Character8[8] = { 0x1f,0x11,0x11,0x11,0x11,0x1b,0x1b,0x1b };    // Headphones
+
 // Global Variables
-unsigned char count;
+unsigned char count, start_count;
 
 // States
 enum St_States{St_SMStart, St_Init, St_Disp, St_Van, St_Wait, St_Pressed, St_Press_Wait} St_state;
@@ -73,6 +73,7 @@ void Start_Screen()
         {
             LCD_ClearScreen();
             count = 0;
+            start_count = 0;
             break;
         }
         case(St_Disp):
@@ -82,24 +83,28 @@ void Start_Screen()
         }
         case(St_Wait):
         {
-            LCD_Cursor(24);
-            
-            if(count % 2)
+            if((start_count % 20) == 0)
             {
-                LCD_Char(0);
-            }
-            else
-            {
-                LCD_Char(1);
-            }
+                LCD_Cursor(24);
             
-            if((~PINA & 0x20) == 0x20)
-            {
-                LCD_ClearScreen();
-                St_state = St_Pressed;
-            }
-            count++;
+                if(count % 2)
+                {
+                    LCD_Char(0);
+                }
+                else
+                {
+                    LCD_Char(1);
+                }
             
+                if((~PINA & 0x20) == 0x20)
+                {
+                    LCD_ClearScreen();
+                    St_state = St_Pressed;
+                }
+                count++;
+            }
+
+            start_count++;
             break;
         }
         case(St_Pressed):
@@ -111,16 +116,21 @@ void Start_Screen()
         }
         case(St_Press_Wait):
         {
-            if(count <= 4)
+            if((start_count % 20) == 0)
             {
+                if(count <= 4)
+                {
                 
-            }
-            else
-            {
-                LCD_ClearScreen();
-            }
+                }
+                else
+                {
+                    LCD_ClearScreen();
+                }
             
-            count++;
+                count++;
+            }
+
+            start_count++;
             break;
         }
         default:
@@ -137,23 +147,23 @@ int main()
     DDRA = 0x0F; PORTA = 0xF0;
     DDRD = 0xFF; PORTD = 0x00;
 
-	LCD_Init();
-    TimerSet(500);
+    LCD_Init();
+    TimerSet(25);
     TimerOn();
-	   
-	LCD_Custom_Char(0, Character1);  /* Build Character1 at position 0 */
-	LCD_Custom_Char(1, Character2);  /* Build Character2 at position 1 */
-	LCD_Custom_Char(2, Character3);  /* Build Character3 at position 2 */
-	LCD_Custom_Char(3, Character4);  /* Build Character4 at position 3 */
-	LCD_Custom_Char(4, Character5);  /* Build Character5 at position 4 */
-	LCD_Custom_Char(5, Character6);  /* Build Character6 at position 5 */
-	LCD_Custom_Char(6, Character7);  /* Build Character6 at position 6 */
-	LCD_Custom_Char(7, Character8);  /* Build Character6 at position 7 */
+    
+    LCD_Custom_Char(0, Character1);  /* Build Character1 at position 0 */
+    LCD_Custom_Char(1, Character2);  /* Build Character2 at position 1 */
+    LCD_Custom_Char(2, Character3);  /* Build Character3 at position 2 */
+    LCD_Custom_Char(3, Character4);  /* Build Character4 at position 3 */
+    LCD_Custom_Char(4, Character5);  /* Build Character5 at position 4 */
+    LCD_Custom_Char(5, Character6);  /* Build Character6 at position 5 */
+    LCD_Custom_Char(6, Character7);  /* Build Character6 at position 6 */
+    LCD_Custom_Char(7, Character8);  /* Build Character6 at position 7 */
 
-	LCD_Command(0x80);		/*cursor at home position */
+    LCD_Command(0x80);		/*cursor at home position */
     LCD_Command(0xc0);
-	
-	while(1)
+    
+    while(1)
     {
         Start_Screen();
         
